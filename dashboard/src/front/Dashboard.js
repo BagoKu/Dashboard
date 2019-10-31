2de9fdf10e5bd40ec5f7d8d04555c3a5c72b46d7
Philippe <philippe.sirvente-maroto-castro@epitech.eu>
2019-10-31 21:17:46 +0100

    [+] Start implementing the dashboard ui

--- dashboard/src/front/Dashboard.js
+++ dashboard/src/front/Dashboard.js
@@ -1,9 +1,36 @@
 import React from "react";
 import {Typography} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline'
 
 function Dashboard() {
     return (
-        <Typography>Dashboard</Typography>
+        <Container>
+            <div>
+                <CssBaseline/>
+                <Drawer variant={'permanent'}
+                >
+                    <div>
+                        <AssignmentIcon/>
+                    </div>
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>{<AddCircleOutline/>}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+            </div>
+        </Container>
     );
 }
 
